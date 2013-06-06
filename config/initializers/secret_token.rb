35a398b54819f8f701924f021ffbe40ce5a77f71
Wysaat <Wysaat@gmail.com>
2013-06-05 23:52:00 -0700

    hello

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlFleet::Application.config.secret_token = '57b65987facc942928db8161367776dae155fe1b21989e91ec0067162d2a773bd375813b6facfa10eb721aac46d9af4b31eb761e9d2832ddf516cbee90456a41'
