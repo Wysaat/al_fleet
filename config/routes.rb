56570261e9785e135cfe94d637727a9313b0cd51
Wysaat <Wysaat@gmail.com>
2013-06-06 07:01:48 -0700

    1

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ AlFleet::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  root :to => 'static_pages#home'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
